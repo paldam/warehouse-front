f170826014807808778f29bb948e4771680670ea
Damian <damianpaluch87@gmail.com>
2019-04-27 01:54:45 +0200

    order audit component

---
+++ src/app/order-audit/order-audit.component.ts
@@ -0,0 +1,80 @@
+import {Component, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
+import {Order} from "../model/order.model";
+import {Checkbox} from "primeng/primeng";
+import {OrderService} from "../order/order.service";
+import {File} from "../model/file";
+import {ActivatedRoute} from "@angular/router";
+
+
+@Component({
+  selector: 'app-order-audit',
+  templateUrl: './order-audit.component.html',
+  styleUrls: ['./order-audit.component.css'],
+    encapsulation: ViewEncapsulation.None
+})
+export class OrderAuditComponent implements OnInit {
+
+    public orderStateBeforeChange : Order = new Order();
+    public orderStateAfterChange : Order = new Order();
+    public revIdOfOrderStateAfterChange :number ;
+    public isDataFetchCompleteForAfterOrderState: Promise<boolean> = Promise.resolve(false);
+    public isDataFetchCompleteForBeforeOrderState: Promise<boolean> = Promise.resolve(false);
+
+    public fileList: File[]=[];
+
+    public additionalSaleCheckbox: boolean = false;
+    public additionalSaleCheckboxForAfterOrderPanel: boolean = false;
+  constructor(orderService: OrderService, activatedRoute :ActivatedRoute) {
+
+
+      this.revIdOfOrderStateAfterChange  = +activatedRoute.snapshot.paramMap.get('revId');// "+" parse to number
+
+
+
+      orderService.getOrderPrevStateFromHistoryByRevId(this.revIdOfOrderStateAfterChange).subscribe(
+          data => {
+              this.orderStateBeforeChange = data[0];
+          },
+          null,
+          () => {
+              this.isDataFetchCompleteForBeforeOrderState = Promise.resolve(true) ;
+              this.orderStateBeforeChange.orderTotalAmount /=100 ;
+              this.orderStateBeforeChange.cod /=100;
+
+              setTimeout(() => {
+
+                  this.additionalSaleCheckbox = this.orderStateBeforeChange.additionalSale == 1;
+              },500 );
+          });
+
+      orderService.getOrderStateFromHistoryByRevId(this.revIdOfOrderStateAfterChange).subscribe(
+          data => {
+              this.orderStateAfterChange = data[0];
+          },
+          null,
+          () => {
+              this.isDataFetchCompleteForAfterOrderState = Promise.resolve(true) ;
+              this.orderStateAfterChange.orderTotalAmount /=100 ;
+              this.orderStateBeforeChange.cod /=100;
+
+              setTimeout(() => {
+                  this.additionalSaleCheckboxForAfterOrderPanel = this.orderStateAfterChange.additionalSale == 1;
+              },500 );
+          });
+
+
+
+
+
+      
+
+
+  }
+
+  ngOnInit() {
+
+
+
+  }
+
+}
